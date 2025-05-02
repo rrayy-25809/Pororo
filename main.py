8d00eb8e2001d66a09e2e425fdd155fb2d29492c
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-05-02 11:23:49 +0900

    Changes to be committed: 기본 틀 잡기 	new file:   .gitignore 	modified:   README.md 	new file:   client/index.html 	new file:   client/src/main.js 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__, template_folder='/client', static_folder='/client/src')
+app.secret_key = 'LN$oaYB9-5KBT7G'
+
+@flask.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
